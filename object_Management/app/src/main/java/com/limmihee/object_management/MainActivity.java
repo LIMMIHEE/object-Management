22eaf42da621badb81d1830986636b65161e9ae3
mirimhee <48482259+LIMMIHEE@users.noreply.github.com>
2020-05-02 16:55:48 +0900

    Fab 대신 일반 버튼으로.

--- object_Management/app/src/main/java/com/limmihee/object_management/MainActivity.java
+++ object_Management/app/src/main/java/com/limmihee/object_management/MainActivity.java
@@ -2,14 +2,66 @@ package com.limmihee.object_management;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.SearchView;
+import android.widget.Toast;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
-    
+    private SearchView searchView ;
+    private Context context;
+    private Button button;
+    //private FloatingActionButton floatingActionButton;
+    private Animation fab_open, fab_close;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        context=getApplicationContext();
+        button = (Button)findViewById(R.id.add_btn);
+        fab_open = AnimationUtils.loadAnimation(context,R.anim.fab_open);
+        fab_close = AnimationUtils.loadAnimation(context,R.anim.fab_close);
+
+//        floatingActionButton = (FloatingActionButton) findViewById(R.id.floatingActionButton);
+//        floatingActionButton.setOnClickListener(new FloatingActionButton(
+//                new Fl
+//        ));
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),object_add.class);
+                startActivity(intent);
+            }
+        });
+
+        searchView = (SearchView) findViewById(R.id.searchView);
+        searchView.setOnQueryTextListener(
+                new SearchView.OnQueryTextListener(){
+                    @Override
+                    public boolean onQueryTextSubmit(String query){
+                        // 검색버튼 눌렀을 때 이벤트
+                        Toast.makeText(MainActivity.this, "검색 처리됨 : " + query, Toast.LENGTH_SHORT).show();
+                        return true;
+                    }
+
+                    @Override
+                    public boolean onQueryTextChange(String newText) {
+                        // 검색어가 변경 됬을 때 이벤트 처리
+                        return false;
+                    }
+
+        });
+
+
     }
+
 }
+
