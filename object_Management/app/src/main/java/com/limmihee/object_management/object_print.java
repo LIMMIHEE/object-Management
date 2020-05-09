58f6a67d40236197370a77b4269a6ba717029c60
mirimhee <48482259+LIMMIHEE@users.noreply.github.com>
2020-05-09 22:08:14 +0900

    Json 파일 수정, 데이터 불러오기

--- object_Management/app/src/main/java/com/limmihee/object_management/object_print.java
+++ object_Management/app/src/main/java/com/limmihee/object_management/object_print.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.res.AssetManager;
 import android.os.Bundle;
+import android.widget.RatingBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -16,6 +17,9 @@ import java.io.InputStreamReader;
 
 public class object_print extends AppCompatActivity {
     private TextView name;
+    private TextView info;
+    private TextView link;
+    private RatingBar ratingBar;
     String title;
 
     @Override
@@ -24,6 +28,9 @@ public class object_print extends AppCompatActivity {
         setContentView(R.layout.activity_object_print);
 
         name = (TextView)findViewById(R.id.textView2) ;
+        info = (TextView)findViewById(R.id.textView4) ;
+        link= (TextView)findViewById(R.id.textView10) ;
+        ratingBar= (RatingBar)findViewById(R.id.ratingBar);
 
         AssetManager assetManager = getResources().getAssets();
         try {
@@ -50,9 +57,10 @@ public class object_print extends AppCompatActivity {
             JSONObject jsonObject =  new JSONObject(jString);
             JSONArray jArr = new JSONArray(jsonObject.getString("OBJ"));
             String btnTitle [] = new String[jArr.length()];
-            title =  jArr.getJSONObject(0).getString("name").toString();
-
-
+            title =  jArr.getJSONObject(0).getString("name");
+            //ratingBar.setRating(Integer.parseInt(jArr.getJSONObject(0).getString("ratingBar")));
+            info.setText(jArr.getJSONObject(0).getString("text"));
+            link.setText(jArr.getJSONObject(0).getString("link"));
 
         }
         catch(JSONException je){
@@ -61,6 +69,8 @@ public class object_print extends AppCompatActivity {
             Toast.makeText(object_print.this, " " + e, Toast.LENGTH_SHORT).show();
         }
         name.setText(title);
+//        ratingBar.setRating(0.5f);
+        ratingBar.setRating(1);
     }
 
 }
